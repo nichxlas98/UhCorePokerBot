a3546b03cb2950e742c221de733686f2dafb6086
nichxlas98 <djflames50@gmail.com>
2024-01-05 11:46:31 -0500

    tweaked /games to utilize config maxPlayers var

--- src/commands/poker/games.ts
+++ src/commands/poker/games.ts
@@ -1,15 +1,18 @@
 import { MessageEmbed } from "discord.js";
 import PokerTable from "../../models/PokerTable";
 import { Command } from "../../structures/Command";
+import ConfigurationManager from "../../managers/ConfigManager";
 
 export default new Command({
     name: "games",
     description: "List all poker games.",
     run: async ({ interaction }) => {
         await interaction.deleteReply();
+
+        const config = new ConfigurationManager().loadConfig();
         let rooms: string = '*';
         for (const table of PokerTable.getTables().asArray()) {
-            rooms += `*Room ID**: ${table.gameId}\n**Game State**: ${table.gameState}\n**Players**: ${table.joined.length()}/8` + '\n*';
+            rooms += `*Room ID**: ${table.gameId}\n**Game State**: ${table.gameState}\n**Players**: ${table.joined.length()}/${config.maxPlayers}` + '\n*';
         }
 
         if (rooms === '*') {
