ae0400ec70dfeebbdfe78be8e954b50b506dd2d6
nichxlas98 <djflames50@gmail.com>
2024-01-05 11:45:53 -0500

    tweaked /creategame to use gameCreation setting

--- src/commands/poker/creategame.ts
+++ src/commands/poker/creategame.ts
@@ -3,7 +3,8 @@ import PokerTable from "../../models/PokerTable";
 import PokerUser from "../../models/PokerUser";
 import { Command } from "../../structures/Command";
 import { getErrorEmbed } from "../../utils/MessageUtils";
-import { Logger } from "../../logs/Logger";
+import { LogManager } from "../../managers/LogManager";
+import ConfigurationManager from "../../managers/ConfigManager";
 
 export default new Command({
     name: "creategame",
@@ -14,6 +15,11 @@ export default new Command({
         const user = PokerUser.findUserByUserId(interaction.user.id);
         if (!user || !user.verified) {
             return interaction.followUp({ embeds: [ getErrorEmbed('Your account must be verified to create a poker game.') ], ephemeral: true });
+        }
+
+        const config = new ConfigurationManager().loadConfig();
+        if (!(interaction.member.permissions.has("ADMINISTRATOR")) && config.gameCreation === false) {
+            return interaction.followUp({ embeds: [ getErrorEmbed('You do not have permission to use this command.') ], ephemeral: true });
         }
 
         const guild = interaction.guild;
@@ -33,7 +39,7 @@ export default new Command({
             new PokerTable(gameId, channel);
         });
 
-        Logger.getInstance().log(`Poker game created: ${gameId} by ${user.userName}`, 1);
+        LogManager.getInstance().log(`Poker game created: ${gameId} by ${user.userName}`, 1);
         return interaction.followUp({ content: 'Poker game created! Join using ID: ' + gameId, ephemeral: true });
     },
 });
